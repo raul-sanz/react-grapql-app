3e7f1655ce4430e67139488629382a810872cf41
raul sanz <estraulsanz@gmail.com>
2019-07-15 18:40:55 -0500

    add routes and pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import { BrowserRouter as Router, Route,Switch } from 'react-router-dom';
 
 import Dashboard from './components/Dashboard'
+import CreateAuthor from './components/authorComponents/CreateAuthor'
+import UpdateAuthor from './components/authorComponents/UpdateAuthor'
+import CreateQuote from './components/quoteComponents/CreateQuote'
+import UpdateQuote from './components/quoteComponents/UpdateQuote'
 
 function App() {
   return (
@@ -10,9 +14,11 @@ function App() {
       <div>
         <Switch>
           <Route exact path='/' component={Dashboard} />
+          <Route exact path='/author/create' component={CreateAuthor} />
+          <Route exact path='/author/update/:id' component={UpdateAuthor} />
+          <Route exact path='/quote/create' component={CreateQuote} />
+          <Route exact path='/quote/update/:id' component={UpdateQuote} />
         </Switch>
-          
-        
       </div>
     </Router>
   );
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -13,47 +13,54 @@ const Dashboard = () => {
           return(
             <div className="container mt-5" >
               <div className="row">
-                <table className="table col-md-4">
-                  <thead>
-                    <tr>
-                      <th scope="col">Nombre</th>
-                      <th scope="col">Edit</th>
-                      <th scope="col">Delete</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                  {data.authors.map((author, index)=>(
-                    <tr key={index}>
-                      <td>{author.name}</td>
-                      <td><button type="button" className="btn btn-primary" >Actualizar</button></td>
-                      
-                      <td><button type="button" className="btn btn-danger" >Eliminar</button></td>
-                    </tr>
-                  ))}
-                  </tbody>
-                </table>
+                <div className=" col-md-4">
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Edit</th>
+                        <th scope="col">Delete</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    {data.authors.map((author, index)=>(
+                      <tr key={index}>
+                        <td>{author.name}</td>
+                        <td><button type="button" className="btn btn-primary" >Actualizar</button></td>
+                        
+                        <td><button type="button" className="btn btn-danger" >Eliminar</button></td>
+                      </tr>
+                    ))}
+                    </tbody>
+                  </table>
+                  <button className="btn btn-success btn-block">Crear Autor</button>
+                </div>
+                
 
-                <table className="table col-md-7 offset-md-1">
-                  <thead>
-                    <tr>
-                      <th scope="col">Nombre</th>
-                      <th scope="col">Author</th>
-                      <th scope="col">Edit</th>
-                      <th scope="col">Delete</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                  {data.quotes.map((quote, index)=>(
-                    <tr key={index}>
-                      <td>{quote.text}</td>
-                      <td>{quote.author.name}</td>
-                      <td><button type="button" className="btn btn-primary" >Actualizar</button></td>
-                      
-                      <td><button type="button" className="btn btn-danger" >Eliminar</button></td>
-                    </tr>
-                  ))}
-                  </tbody>
-                </table>
+                <div className="col-md-7 offset-md-1">
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Author</th>
+                        <th scope="col">Edit</th>
+                        <th scope="col">Delete</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    {data.quotes.map((quote, index)=>(
+                      <tr key={index}>
+                        <td>{quote.text}</td>
+                        <td>{quote.author.name}</td>
+                        <td><button type="button" className="btn btn-primary" >Actualizar</button></td>
+                        
+                        <td><button type="button" className="btn btn-danger" >Eliminar</button></td>
+                      </tr>
+                    ))}
+                    </tbody>
+                  </table>
+                  <button className="btn btn-info btn-block">Crear Frase</button>
+                </div>
               </div>
             </div>
           )
